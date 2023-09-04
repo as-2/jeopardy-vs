77af5ce77162147c353b0a9f93a4c4ae5d54548c
as-2 <alaysiastone@gmail.com>
2023-09-04 16:21:33 -0400

    able to parse serial output of button press
    parses color of button pressed and prints out the correct one

--- test/trial-arduino-comm.js
+++ test/trial-arduino-comm.js
@@ -1,27 +1,23 @@
 console.log('HELLO WORLD');
 const { SerialPort } = require('serialport');
 console.log("SERIAL PORT");
-// const { ReadlineParser } = require('@serialport/parser-readline');
+const { ReadlineParser } = require('@serialport/parser-readline');
 
-// const port = new SerialPort('/dev/tty.usbmodem144301', { baudRate: 9600 }); // Replace 'COMx' with your Arduino's port
 const port = new SerialPort({
     path: '/dev/cu.usbmodem144301',
     baudRate: 9600,
   })
 console.log("PORT");
-// const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
 
-// parser.on('data', (data) => {
-//     // Handle data received from Arduino here
-//     if (data === 'YELLOW') {
-//       console.log('OMG BLUE PRESSED');
-//     } else if (data === 'GREEN') {
-//         console.log('OMG GREEN PRESSED');
-//     } else if (data === 'BLUE') {
-//         console.log('OMG GREEN PRESSED');
-//     }
-//   });
-port.on('data', function (data) {
-    console.log('Data:', data)
+parser.on('data', (data) => {
+    // Handle data received from Arduino here
+    if (data === 'YELLOW') {
+      console.log('OMG YELLOW PRESSED');
+    } else if (data === 'GREEN') {
+        console.log('OMG GREEN PRESSED');
+    } else if (data === 'BLUE') {
+        console.log('OMG BLUE PRESSED');
+    }
   });
